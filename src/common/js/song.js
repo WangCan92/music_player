bd65139f40bd439cc467111bd47ce909bef34755
wangcan <wangcan@jingdata.com>
2019-05-10 19:08:41 +0800

    first commit

---
+++ src/common/js/song.js
@@ -0,0 +1,77 @@
+import {getLyric} from 'api/song'
+import {ERR_OK} from 'api/config'
+import {Base64} from 'js-base64'
+import jsonp from 'common/js/jsonp'
+
+export default class Song {
+  constructor({id, mid, singer, name, album, duration, image, url}) {
+    this.id = id
+    this.mid = mid
+    this.singer = singer
+    this.name = name
+    this.album = album
+    this.duration = duration
+    this.image = image
+    this.url = url
+  }
+
+  getLyric() {
+    if (this.lyric) {
+      return Promise.resolve(this.lyric)
+    }
+
+    return new Promise((resolve, reject) => {
+      getLyric(this.mid).then((res) => {
+        if (res.retcode === ERR_OK) {
+          this.lyric = Base64.decode(res.lyric)
+          resolve(this.lyric)
+        } else {
+          reject('no lyric')
+        }
+      })
+    })
+  }
+}
+
+export function getSongVkey (songmid) { // 获取歌曲的vkey
+  const url = 'https://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg'
+  const data = Object.assign({}, {
+    callback: 'musicJsonCallback',
+    loginUin: 3051522991,
+    format: 'jsonp',
+    platform: 'yqq',
+    needNewCode: 0,
+    cid: 205361747,
+    uin: 3051522991,
+    guid: 5931742855,
+    songmid: songmid,
+    filename: `C400${songmid}.m4a`
+  })
+
+  return jsonp(url, data)
+}
+
+export function createSong(musicData, vkey) {
+  return new Song({
+    id: musicData.songid,
+    mid: musicData.songmid,
+    singer: filterSinger(musicData.singer),
+    name: musicData.songname,
+    album: musicData.albumname,
+    duration: musicData.interval,
+    image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
+    url: `http://dl.stream.qqmusic.qq.com/C400${musicData.songmid}.m4a?fromtag=38&guid=5931742855&vkey=${vkey}`
+  })
+}
+
+function filterSinger(singer) {
+  let ret = []
+  if (!singer) {
+    return ''
+  }
+  singer.forEach((s) => {
+    ret.push(s.name)
+  })
+  return ret.join('/')
+}
+
---
+++ src/components/top-list/top-list.vue
@@ -0,0 +1,78 @@
+<template>
+  <transition name="slide">
+    <music-list :rank="rank" :title="title" :bg-image="bgImage" :songs="songs"></music-list>
+  </transition>
+</template>
+
+<script type="text/ecmascript-6">
+  import MusicList from 'components/music-list/music-list'
+  import {getMusicList} from 'api/rank'
+  import {ERR_OK} from 'api/config'
+  import {mapGetters} from 'vuex'
+  import {createSong, getSongVkey} from 'common/js/song'
+
+  export default {
+    computed: {
+      title() {
+        return this.topList.topTitle
+      },
+      bgImage() {
+        if (this.songs.length) {
+          return this.songs[0].image
+        }
+        return ''
+      },
+      ...mapGetters([
+        'topList'
+      ])
+    },
+    data() {
+      return {
+        songs: [],
+        rank: true
+      }
+    },
+    created() {
+      this._getMusicList()
+    },
+    methods: {
+      _getMusicList() {
+        if (!this.topList.id) {
+          this.$router.push('/rank')
+          return
+        }
+        getMusicList(this.topList.id).then((res) => {
+          if (res.code === ERR_OK) {
+            this.songs = this._normalizeSongs(res.songlist)
+          }
+        })
+      },
+      _normalizeSongs(list) {
+        let result = []
+        list.forEach((musicData) => {
+          // console.log('item',item)
+          // 解构赋值-拿到item 下的 musicData 列表数据
+          // let {musicData} = item.data
+          getSongVkey(musicData.data.songmid).then((res) => {
+            const vkey = res.data.items[0].vkey
+            if (musicData.data.songid && musicData.data.albummid) {
+              result.push(createSong(musicData.data, vkey))
+            }
+          })
+        })
+        return result
+      }
+    },
+    components: {
+      MusicList
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .slide-enter-active, .slide-leave-active
+    transition: all 0.3s ease
+
+  .slide-enter, .slide-leave-to
+    transform: translate3d(100%, 0, 0)
+</style>
